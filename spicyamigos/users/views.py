570e95cc854b26047aa8ddb6b3b7ffc9303a3551
ryonistic <ryonistic@protonmail.com>
2022-04-01 17:06:15 +0530

    Apr 1 5:06 PM commit

--- spicyamigos/users/views.py
+++ spicyamigos/users/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login as logthemin, logout as logthemout
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def login(request):
@@ -26,4 +27,19 @@ def logout(request):
     return redirect('login')
 
 def signup(request):
-    return render(request, 'signup.html', {})
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password = password)
+            logthemin(request, user)
+            messages.success(request, 'Account Created. You are now logged in')
+            return redirect('store_home')
+        else:
+            messages.success(request, 'There was an error. Please enter valid details')
+            #return redirect('signup')
+    else:
+        form = UserRegisterForm
+    return render(request, 'signup.html', {'form':form})
