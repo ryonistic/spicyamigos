0c67d8cabf7dd9aa4f5757f95c99b25f00dd4afa
ryonistic <ryonistic@protonmail.com>
2022-04-02 08:08:34 +0530

    Apr 2 8:08 AM commit

--- spicyamigos/users/models.py
+++ spicyamigos/users/models.py
@@ -1,10 +1,11 @@
 """User related models defined here."""
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 
 class User(AbstractUser):
     """Custom user model defined here."""
-    pass
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     def __str__(self):
         return str(self.username)
