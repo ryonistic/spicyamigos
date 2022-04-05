ad2c6a6338f2f67caf922a7c9aba46e60995fcbc
ryonistic <ryonistic@protonmail.com>
2022-04-05 19:13:45 +0530

    SSH commit

--- frontend/pages/menu.js
+++ frontend/pages/menu.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import Link from 'next/link'
-export default function Home(){
+
+export default function Menu(){
     return (
     <>
-        <Link href="/"><a>Home</a></Link>{' '}
+        <Head>
+            <title>Menu</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Link href="/"><a>Back to Home</a></Link>{' '}
         <Link href="/main/main"><a>Main</a></Link>
     <h1>Hello, world!</h1>
       </>
