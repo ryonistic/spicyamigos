b80c51f1e8610cd845128ed40a355d497cc38111
ryonistic <ryonistic@protonmail.com>
2022-04-07 18:48:18 +0530

    Commit after adding nextjs frontend

--- config/settings.py
+++ config/settings.py
@@ -135,9 +135,9 @@ AUTH_USER_MODEL = 'users.User'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'spicyamigos', 'static'),]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'spicyamigos', 'static'),]
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
